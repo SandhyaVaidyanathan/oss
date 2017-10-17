9ae172503a2a8a13cea3c5b723fe751ff3f017e6
SandhyaVaidyanathan <o2-vaidyanathan@hoare7.cs.umsl.edu>
2017-10-16 20:53:22 -0500

    clock-1

--- oss.c
+++ oss.c
@@ -17,11 +17,17 @@
 
 int spawnedSlaves = 0;
 pid_t childpid, gpid;
+int shmid;
+shmClock *shinfo;
+
 const int ZTIME_DEFAULT = 20;
 const int MAXSLAVE_DEFAULT = 5;
+const int MAXSLAVES = 100;
+const long long int NANOSECOND = 1000000000; 
 
 void spawnSlaveProcess(int);
 void interruptHandler(int);
+void myClock();
 
 int main(int argc, char const *argv[])
 {
@@ -29,9 +35,8 @@ int main(int argc, char const *argv[])
 	logfile = "log.txt";
 	int option = 0;	
 	int slaveProcess = 0,ztime = 0 ;
-	int shmid;
-	shmClock *shinfo;
-	key_t key;
+
+	key_t clock_key, msg_key;
 
 	if (argc < 2){ // check for  command-line arguments		
   		fprintf(stderr, " %s: Error : Try Executable -h for help \n",argv[0]);		
@@ -99,9 +104,9 @@ int main(int argc, char const *argv[])
 	signal(SIGALRM, interruptHandler);
 	alarm(ztime);	
 
-	key = 555;
+	clock_key = 555;
 	//Create shared memory segment 
-	shmid = shmget(key, 200*sizeof(shinfo), 0744 |IPC_CREAT |IPC_EXCL);
+	shmid = shmget(key, 20*sizeof(shinfo), 0744 |IPC_CREAT |IPC_EXCL);
 	if ((shmid == -1) && (errno != EEXIST)) /* real error */
 	{
 		perror("Unable to create shared memory");
@@ -122,8 +127,14 @@ int main(int argc, char const *argv[])
 		shinfo->sec = 0;
 	}
 // Open log file 
+
 FILE *fp = fopen(logfile, "a");
+	fprintf(stderr, "Starting the clock..\n" );
+	myClock();
 	spawnSlaveProcess(slaveProcess);
+	// start clock
+
+
 
 
 
@@ -133,25 +144,22 @@ return 0;
 void spawnSlaveProcess(int noOfSlaves)
 {
 	int i;
+	//Forking processes
 	for(i = 0; i < noOfSlaves; i++) 
-	{
-		
-    	printf("Creating Slave process : %d \n", spawnedSlaves);
-    	childpid = fork();
-    if(childpid < 0) 
-    	{
-      		printf("Failed to fork ");
-      		break;
-    	}
+	{ 
+       	if((childpid = fork())<=0)
+       		break;
+    }
+
     if (childpid == 0)
 	    {
-    	//execl user.c
-	    	gpid = getpgrp();
-			printf("exec %d\n",i);    
+    	//execl user.c    	
+			fprintf(stderr,"exec %d\n",i);   
+
     	}
     	spawnedSlaves++;
-	}
 }
+
 
 void interruptHandler(int SIG){
   signal(SIGQUIT, SIG_IGN);
@@ -170,3 +178,22 @@ void interruptHandler(int SIG){
 	kill(-getpgrp(), SIGQUIT);
 }
 
+void myClock()
+{
+	while(shinfo->nsec < 2*NANOSECOND)
+	{
+
+		shinfo->nsec = (shinfo->nsec+2000000);
+		shinfo->sec = (shinfo->nsec/NANOSECOND);
+		long nano;
+		nano = shinfo->nsec;
+		if (shinfo->nsec >= NANOSECOND)
+		{
+			nano = shinfo->nsec - NANOSECOND;
+			if(nano == NANOSECOND)
+				nano = 0;
+		}
+		fprintf(stderr, "Seconds: %ld Nanoseconds: %ld \n",shinfo->sec,nano );
+	}
+	fprintf(stderr, "Clock ending after 2 seconds.\n" );
+}
